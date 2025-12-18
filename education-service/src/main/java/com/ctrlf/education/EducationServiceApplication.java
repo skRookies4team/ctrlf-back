0a1cc4fd7fe51076a2510ad0d0c8e3cea167fee8
yunjongyun <yunyami0605@gmail.com>
2025-12-18 23:11:20 +0900

    fix(education): 빌드에러해결
    1. 엔티티 스캔 추가

--- education-service/src/main/java/com/ctrlf/education/EducationServiceApplication.java
+++ education-service/src/main/java/com/ctrlf/education/EducationServiceApplication.java
@@ -3,12 +3,16 @@ package com.ctrlf.education;
 import com.ctrlf.common.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @Import(SecurityConfig.class)
 @EnableFeignClients
+@EntityScan(basePackages = "com.ctrlf.education")
+@EnableJpaRepositories(basePackages = "com.ctrlf.education")
 public class EducationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EducationServiceApplication.class, args);
