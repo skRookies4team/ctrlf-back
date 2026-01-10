009a63ee14c1d9557e740d685ba0fba7a5664f99
hia1394 <a4737465@naver.com>
2026-01-10 19:27:19 +0900

    hia

--- chat-service/src/main/java/com/ctrlf/chat/faq/controller/FaqDashboardController.java
+++ chat-service/src/main/java/com/ctrlf/chat/faq/controller/FaqDashboardController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/faq/dashboard")
+@RequestMapping("/api/faq/dashboard")
 public class FaqDashboardController {
 
     private final FaqDashboardService faqDashboardService;
