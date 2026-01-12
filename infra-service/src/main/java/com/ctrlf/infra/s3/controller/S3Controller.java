e151b724ee2cb03a1db427941ac7aa52d47d5f18
hia1394 <a4737465@naver.com>
2026-01-12 10:25:44 +0900

    s3

--- infra-service/src/main/java/com/ctrlf/infra/s3/controller/S3Controller.java
+++ infra-service/src/main/java/com/ctrlf/infra/s3/controller/S3Controller.java
@@ -30,7 +30,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 
 @RestController
-@RequestMapping("/api-infra/files/presign")
+@RequestMapping("/api-infra/infra/files/presign")
 @RequiredArgsConstructor
 @Tag(name = "Infra - S3", description = "S3 Presigned URL API")
 public class S3Controller {
